dbd09d3b8a300ae0a7c3486a094ae9818a168041
Walls <jyron.dev@gmail.com>
2021-05-02 15:47:43 -0500

    Added Auth0
    Moved Auth0 PR from @adriandelgg into current repo.
    
    Co-Authored-By: Adrian Delgado <32179921+adriandelgg@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,107 +1,106 @@
-// import React from "react";
-// import LoginBar from "./components/LoginBar";
-// import Stocks from "./components/Stocks";
-
-// function App() {
-//   return (
-//     <div>
-//       <LoginBar />
-//       <h1><a href='/'>StockRanker</a></h1>
-//       <h1>S&P 500</h1>
-//       <Stocks />
-//     </div>
-//   );
-// }
-
-// export default App;
-////////////////////////////////////////////////////////////////////
-
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios';
+import LoginBar from './components/LoginBar';
+import Stocks from './components/Stocks';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const App = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [user, setUser] = useState();
+	console.log(useAuth0());
+	const { isLoading } = useAuth0();
 
-  useEffect(() => {
-    const loggedInUser = localStorage.getItem("user");
-    if (loggedInUser) {
-      const foundUser = loggedInUser;
-      setUser(foundUser);
-    }
-  }, []);
+	// const [username, setUsername] = useState('');
+	// const [password, setPassword] = useState('');
+	// const [user, setUser] = useState();
 
-  // logout the user
-  const handleLogout = () => {
-    setUser({});
-    setUsername("");
-    setPassword("");
-    localStorage.clear();
-  };
+	// useEffect(() => {
+	// 	const loggedInUser = localStorage.getItem('user');
+	// 	if (loggedInUser) {
+	// 		const foundUser = loggedInUser;
+	// 		setUser(foundUser);
+	// 	}
+	// }, []);
 
-  // login the user
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    var formData = new FormData();
-    formData.set("username", username);
-    formData.set("password", password);
-    //const user = { username, password };
-    // send the username and password to the server
-    const response = await axios.post(
-      "http://localhost:8000/auth/jwt/login",
-      formData
-    ).catch((error)=>console.log(error));
-    // set the state of the user
-    setUser(response.data);
-    // store the user in localStorage
-    localStorage.setItem("user", response.data);
-    // get user profile using token
+	// // logout the user
+	// const handleLogout = () => {
+	// 	setUser({});
+	// 	setUsername('');
+	// 	setPassword('');
+	// 	localStorage.clear();
+	// };
 
-    const profile = await axios
-      .get("http://localhost:8000/users/me", {
-        headers: {
-          Authorization: `Bearer ${localStorage.user}`,
-        },
-      })
-      .then((response) => console.log(response))
-      .catch((err) => console.log(err));
-  };
+	// // login the user
+	// const handleSubmit = async e => {
+	// 	e.preventDefault();
+	// 	var formData = new FormData();
+	// 	formData.set('username', username);
+	// 	formData.set('password', password);
+	// 	//const user = { username, password };
+	// 	// send the username and password to the server
+	// 	const response = await axios
+	// 		.post('http://localhost:8000/auth/jwt/login', formData)
+	// 		.catch(error => console.log(error));
+	// 	// set the state of the user
+	// 	setUser(response.data);
+	// 	// store the user in localStorage
+	// 	localStorage.setItem('user', response.data);
+	// 	// get user profile using token
 
-  // if there's a user show the message below
-  if (user) {
-    return (
-      <div>
-        {user.name} is loggged in
-        <button onClick={handleLogout}>logout</button>
-      </div>
-    );
-  }
+	// 	const profile = await axios
+	// 		.get('http://localhost:8000/users/me', {
+	// 			headers: {
+	// 				Authorization: `Bearer ${localStorage.user}`
+	// 			}
+	// 		})
+	// 		.then(response => console.log(response))
+	// 		.catch(err => console.log(err));
+	// };
 
-  // if there's no user, show the login form
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Username: </label>
-        <input
-          type="text"
-          value={username}
-          placeholder="enter a username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-        <div>
-          <label htmlFor="password">password: </label>
-          <input
-            type="password"
-            value={password}
-            placeholder="enter a password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+	// // if there's a user show the message below
+	// if (user) {
+	// 	return (
+	// 		<div>
+	// 			{user.name} is loggged in
+	// 			<button onClick={handleLogout}>logout</button>
+	// 		</div>
+	// 	);
+	// }
+
+	if (isLoading) return <div>Loading...</div>;
+
+	return (
+		<>
+			<LoginButton />
+			<LogoutButton />
+			<Profile />
+			<Stocks />
+
+			{/* <div>
+				<form onSubmit={handleSubmit}>
+					<label htmlFor="username">Username: </label>
+					<input
+						type="text"
+						value={username}
+						placeholder="enter a username"
+						onChange={({ target }) => setUsername(target.value)}
+					/>
+					<div>
+						<label htmlFor="password">password: </label>
+						<input
+							type="password"
+							value={password}
+							placeholder="enter a password"
+							onChange={({ target }) => setPassword(target.value)}
+						/>
+					</div>
+					<button type="submit">Login</button>
+				</form>
+			</div> */}
+		</>
+	);
 };
 
 export default App;
--- frontend/src/components/Stocks.jsx
+++ frontend/src/components/Stocks.jsx
@@ -1,75 +1,77 @@
-import React, { useEffect, useState } from "react";
-import Box from "@material-ui/core/Box";
-import { Container, Button, Grid } from "@material-ui/core";
-//import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
-// import ThumbDownAltIcon from "@material-ui/icons/ThumbDownAlt";
+import React, { useEffect, useState } from 'react';
+import { Container, Button, Grid, Card } from '@material-ui/core';
+import { ThumbUpAlt, ThumbDownAlt } from '@material-ui/icons';
+import { useAuth0 } from '@auth0/auth0-react';
 
-const vote_url = "http://localhost:8000/vote";
+const vote_url = 'http://localhost:8000/vote';
 const StocksContext = React.createContext({
-  stocks: [],
-  fetchStocks: () => {},
+	stocks: [],
+	fetchStocks: () => {}
 });
 
 export default function Stocks() {
-  const [stocks, setStocks] = useState([]);
-  const fetchStocks = async () => {
-    const response = await fetch("http://localhost:8000/stocks");
-    const stocks = await response.json();
-    setStocks(stocks.data);
-  };
-  useEffect(() => {
-    fetchStocks();
-  }, []);
-  function createVote(votetype, stock, e) {
-    e.preventDefault();
-    if (!localStorage.user) {
-      return "Login!";
-    }
-    const data = JSON.stringify({
-      symbol: stock.symbol,
-      votetype: votetype,
-    });
+	const { isAuthenticated } = useAuth0();
+	const [stocks, setStocks] = useState([]);
 
-    fetch(vote_url, {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-        Authorization: "Bearer " + localStorage.token,
-      },
-      body: data,
-    })
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
-  }
+	const fetchStocks = async () => {
+		const response = await fetch('http://localhost:8000/stocks');
+		const stocks = await response.json();
+		console.log(stocks);
+		setStocks(stocks.data);
+	};
 
-  return (
-    <StocksContext.Provider value={{ stocks, fetchStocks }}>
-      <Container>
-        <Grid container>
-          <div>
-            {stocks.map((stock) => (
-              <Box key={stock.symbol}>
-                <p>
-                  {stock.name} {stock.symbol} ${stock.price}
-                </p>
-                <Button
-                  style={{ color: "green" }}
-                  onClick={(e) => createVote(true, stock, e)}
-                >
-                  {stock.likes}
-                </Button>
-                /
-                <Button
-                  style={{ color: "red" }}
-                  onClick={(e) => createVote(false, stock, e)}
-                >
-                  {stock.dislikes}
-                </Button>
-              </Box>
-            ))}
-          </div>
-        </Grid>
-      </Container>
-    </StocksContext.Provider>
-  );
+	useEffect(() => {
+		fetchStocks();
+	}, []);
+
+	function createVote(votetype, stock, e) {
+		e.preventDefault();
+		// if (!localStorage.user) {
+		// 	return 'Login!';
+		// }
+		const data = JSON.stringify({
+			symbol: stock.symbol,
+			votetype
+		});
+
+		fetch(vote_url, {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json',
+				Authorization: 'Bearer ' + localStorage.token
+			},
+			body: data
+		})
+			.then(res => console.log(res))
+			.catch(err => console.log(err));
+	}
+
+	return (
+		<StocksContext.Provider value={{ stocks, fetchStocks }}>
+			<Grid container component={Container} spacing={3}>
+				{stocks.map(stock => (
+					<Grid item component={Card} lg={3} sm={6} key={stock.symbol}>
+						<p>
+							{stock.name} {stock.symbol} ${stock.price}
+						</p>
+						<Button
+							style={{ color: 'green' }}
+							onClick={e => createVote(true, stock, e)}
+							startIcon={<ThumbUpAlt />}
+						>
+							{stock.likes}
+						</Button>
+						/
+						<Button
+							style={{ color: 'red' }}
+							onClick={e => createVote(false, stock, e)}
+							endIcon={<ThumbDownAlt />}
+						>
+							{stock.dislikes}
+						</Button>
+					</Grid>
+				))}
+			</Grid>
+		</StocksContext.Provider>
+	);
 }
